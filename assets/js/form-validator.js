3de60479518f1a43e1bee9c5eb97c90197a1803b
heyalexluna <heyalexluna@gmail.com>
2020-12-08 08:46:21 +0800

    Update to version 2.0

---
+++ assets/js/form-validator.js
@@ -0,0 +1,25 @@
+/*------------------------------------------------------------------
+* Bootstrap Simple Admin Template
+* Version: 2.0
+* Author: Alexis Luna
+* Copyright 2020 Alexis Luna
+* Website: https://github.com/alexis-luna/bootstrap-simple-admin-template
+-------------------------------------------------------------------*/
+// Starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+/*------------------------------------------------------------------
+* Bootstrap Simple Admin Template
+* Version: 2.0
+* Author: Alexis Luna
+* Copyright 2020 Alexis Luna
+* Website: https://github.com/alexis-luna/bootstrap-simple-admin-template
+-------------------------------------------------------------------*/
+// Toggle sidebar on Menu button click
+$('#sidebarCollapse').on('click', function() {
+    $('#sidebar').toggleClass('active');
+    $('#body').toggleClass('active');
+});
+
+// Auto-hide sidebar on window resize if window size is small
+// $(window).on('resize', function () {
+//     if ($(window).width() <= 768) {
+//         $('#sidebar, #body').addClass('active');
+//     }
+// });
