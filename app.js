920cc27bc3c8ab6e94a7a3d54f4e356d6d46c344
asapsui <samuel.u.imose@gmail.com>
2024-05-02 16:01:05 -0500

    added more stuff

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Importing libraries
-const { getUser, addUser, getUserIngredients, addIngredients } = require('./database.js')
+const { getUser, addUser, getUserIngredients, addIngredient } = require('./database.js')
 const express = require('express');
 const session = require('express-session');
 const path = require('path');
@@ -7,7 +7,7 @@ const path = require('path');
 // Initializing app
 const app = express();
 
-// Creating an express session for passing user info across pages
+// Creating an express session for passing user info across pages (caching)
 app.use(session({
     secret: 'your-secret-key',
     resave: false,
@@ -19,11 +19,17 @@ app.use(session({
 
 const PORT = process.env.PORT || 3300;
 
+// Middleware
+
 // For loading in css and bootstrap
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
+// For parsing incoming requests
 app.use(express.urlencoded({extended: false}));
 
+// Routes
+
+// For handling registration form submission
 app.post("/register", async (req, res) => {
        const username = req.body.username;
        const password = req.body.password;
@@ -50,6 +56,7 @@ app.post("/register", async (req, res) => {
         })
 })
 
+// For handling login form submission
 app.post("/login", async (req, res) => {
        const username = req.body.username;
        const password = req.body.password;
@@ -68,29 +75,59 @@ app.post("/login", async (req, res) => {
         })
 })
 
-// Routes
+// For handling the add ingredient form submission
+app.post("/add-ingredient", async (req, res) => {
+       const name = req.body.ingredientName;
+       const user = req.session.user;
+
+       if (user === undefined) {
+            console.log("Not logged in. Cannot add ingredients.");
+            res.redirect("/login");
+       }
+       else {
+            addIngredient(name, user.Id)
+                .then(newIngredient => {
+                    if (newIngredient === 1) {
+                        console.log("Ingredient added for user.");
+                        res.redirect("/add-ingredient");
+                    }
+                    else {
+                        console.error("Failed to add ingredient for user");
+                        res.redirect("/add-ingredient");
+                    }
+                })
+
+       }
+})
+
+// Route to handle GET requests to the '/login' endpoint
 app.get('/login', (req, res) => {
     res.render("login.ejs")
 })
 
+// Route to handle GET requests to the '/login-error' endpoint
 app.get('/login-error', (req, res) => {
     res.render("login-error.ejs")
 })
 
+// Route to handle GET requests to the '/register' endpoint
 app.get('/register', (req, res) => {
     res.render("register.ejs")
 })
 
+// Route to handle GET requests to the '/register-error' endpoint
 app.get('/register-error', (req, res) => {
     res.render("register-error.ejs")
 })
 
+// Route to handle GET requests to the '/home' endpoint
 app.get('/home', (req, res) => {
     const user = req.session.user;
     console.log(user);
     res.render("home.ejs", { user })
 })
 
+// Route to handle GET requests to the '/pantry' endpoint
 app.get('/pantry', (req, res) => {
     const user = req.session.user;
     if (user === undefined){
@@ -106,6 +143,7 @@ app.get('/pantry', (req, res) => {
     }
 })
 
+// Route to handle GET requests to the '/profile' endpoint
 app.get('/profile', (req, res) => {
     const user = req.session.user;
     if (user === undefined) {
@@ -117,10 +155,12 @@ app.get('/profile', (req, res) => {
     console.log(user);
 })
 
+// Route to handle GET requests to the '/cookbook' endpoint
 app.get('/cookbook', (req, res) => {
     res.render("cookbook.ejs")
 })
 
+// Route to handle GET requests to the '/add-ingredient' endpoint
 app.get('/add-ingredient', (req, res) => {
     res.render("add-ingredient.ejs")
 })
